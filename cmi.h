ec12fec5a62021d684db839f15eccc3f5be8c89a
mrmirror662 <62153832+mrmirror662@users.noreply.github.com>
2021-10-15 20:17:04 +0530

    Add files via upload

---
+++ cmi.h
@@ -0,0 +1,178 @@
+#pragma once
+//includes
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <stdlib.h>
+#include <regex>
+
+//defines
+#define USE_CLASS_NAME "ucn"
+#define NO_VERB 0
+#define NO_INCLUDE_GUARD 1
+#define NO_INCLUDE 2
+#define IMPLE_STRUCT 3
+using line = std::string;
+const std::string FUNCTION_SIGNATURE_REG = "\\s*(\\w*)\\s*(\\w*)\\((.*)\\)\\s*(const)?;";
+
+namespace utils
+{
+    std::string ReadFile(const std::string &filepath)
+    {
+        std::ifstream file;
+        file.open(filepath);
+
+        if (!file.is_open())
+            return "";
+
+        std::stringstream ss;
+
+        while (!file.eof())
+        {
+            std::string temp;
+            std::getline(file, temp);
+            ss << temp << '\n';
+        }
+        return ss.str();
+    }
+
+    std::vector<line> TokeniseStringToLines(const std::string &str)
+    {
+        std::vector<line> lines;
+        std::string temp;
+
+        for (char c : str)
+        {
+            if (c == '\n')
+            {
+                lines.push_back(temp);
+                temp = "";
+                continue;
+            }
+            temp.push_back(c);
+        }
+
+        return lines;
+    }
+}
+struct CppFunction
+{
+    std::string Rtype;
+    std::string Name;
+    std::string Args;
+    std::string ClassName;
+    bool IsConst;
+};
+struct CppClass
+{
+    std::string Name;
+    CppClass(const std::string &_classname) { Name = _classname; }
+    CppClass() {}
+};
+
+class CMI
+{
+private:
+    CppClass m_class;
+    std::vector<CppFunction> m_functions;
+    std::vector<line> m_lines;
+    std::bitset<16> addop;
+
+public:
+    CMI(const std::vector<line> &_lines, std::bitset<16> &_addop)
+    {
+        addop = _addop;
+        m_lines = _lines;
+        ParseLines();
+    }
+    std::vector<CppFunction> GetParsedFunctions() const
+    {
+        return m_functions;
+    }
+    auto GetClassName() { return m_class.Name; }
+
+    void GenerateCMI(const std::vector<std::string> &FormattedFunc, std::string DestFile, std::string HeaderFile = USE_CLASS_NAME)
+    {
+        std::ofstream write;
+        write.open(DestFile, std::ofstream::out | std::ofstream::trunc);
+        std::string include = "#include";
+        char quote = '"';
+        if (HeaderFile == USE_CLASS_NAME)
+        {
+            HeaderFile = m_class.Name;
+        }
+        else
+        {
+            HeaderFile = HeaderFile.substr(0, HeaderFile.size() - 2);
+        }
+
+        const std::string doth = ".h";
+        if (!addop[NO_INCLUDE_GUARD])
+        {
+            write << "#pragma once\n";
+        }
+        if (!addop[NO_INCLUDE])
+        {
+            write << include << quote << HeaderFile << doth << quote << "\n\n";
+        }
+
+        for (auto function : FormattedFunc)
+        {
+            write << function
+                  << "\n{\n\n}\n";
+        }
+        write.close();
+    }
+
+private:
+    void ParseLines()
+    {
+        //check if class is present in file
+        bool ClassPresent = false;
+        std::string CurrentClass;
+
+        for (line &l : m_lines)
+        {
+            std::stringstream ss;
+            if ((l.find("class") != std::string::npos) || (addop[IMPLE_STRUCT] && (l.find("struct") != std::string::npos)))
+            {
+                ClassPresent = true;
+                std::string KeywordClass, ClassName;
+
+                ss << l;
+                ss >> KeywordClass >> ClassName;
+
+                m_class = ClassName;
+                CurrentClass = ClassName;
+            }
+            else
+            {
+                if (l.find('#') == std::string::npos)
+                {
+                    const std::regex reg(FUNCTION_SIGNATURE_REG, std::regex::ECMAScript);
+                    std::smatch match;
+                    if (std::regex_search(l, match, reg) == true)
+                    {
+                        std::string type, name, args;
+                        bool IsConst = false;
+
+                        type = match[1];
+                        name = match[2];
+                        args = match[3];
+
+                        IsConst = match[4] == "const" ? true : false;
+
+                        m_functions.push_back({type, name, args, CurrentClass, IsConst});
+                    }
+                }
+            }
+        }
+        if (!ClassPresent)
+        {
+            std::cout << "class not present in file\n";
+            exit(1);
+        }
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include "cmi.h"
+#include <string.h>
+
+/* arguement defines*/
+enum class OPTIONS
+{
+    MODE_HEADER_FILE_NAME,
+    MODE_IMPLEMENTATON_FILE_NAME,
+    MODE_HELP,
+    MODE_INVALID_ARG,
+    MODE_NO_VERBOSE,
+    MODE_NO_INCLUDE_GUARD,
+    MODE_NO_INCLUDE,
+    MODE_IMPLE_STRUCT
+};
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*Messeges*/
+const std::string INVALIDMSG = "invalid argument , refer -help for more info!\n";
+
+const std::string HELPERMSG =
+    "#### HELP ####\n"
+    "-header : takes header file name\n-imple : takes implementation file name\n "
+    "usage: cmi -header <filename> -imple <filename> <additional options>\n"
+    "additional options:"
+    "\n-noguard -> disables include guard"
+    "\n-noinclude -> doesn't include header file"
+    "\n-implestruct -> implements structs"
+    "\n-novb -> less printing"
+    "\n#### END ####\n";
+/*~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*Unsuccesfull Run define*/
+#define FAILED_EXIT()        \
+    std::cout << INVALIDMSG; \
+    exit(1)
+#define SUCCESS_EXIT() \
+    exit(0)
+
+class application
+{
+private:
+    int m_argc;
+    char **m_argv;
+    std::string HeaderP, ImpleP;
+    std::bitset<16> m_AdditionalOptions;
+
+public:
+    static void Start(int argc, char **argv)
+    {
+        application app(argc, argv);
+        app.getInput();
+        app.WriteToFile();
+    }
+
+private:
+    application(int argc, char **argv) : m_argc(argc), m_argv(argv) {}
+    void getInput()
+    {
+        using namespace std;
+        if (m_argc == 1)
+        {
+            FAILED_EXIT();
+        }
+        else
+        {
+            for (int i = 1; i < m_argc; i++)
+            {
+                char *Curr = m_argv[i];
+                if (strlen(Curr) > 512 || m_argc > 32)
+                {
+                    FAILED_EXIT();
+                }
+                if (!IsArg(Curr))
+                {
+                    continue;
+                }
+                std::string SwitchMode;
+                std::string Val;
+
+                if (m_argv[i + 1] == nullptr)
+                {
+                    Val = " ";
+                }
+                else
+                {
+                    Val = m_argv[i + 1];
+                }
+                SwitchMode = (Curr + 1);
+                auto code = ProcessArguements(SwitchMode, Val);
+                switch (code)
+                {
+                case OPTIONS::MODE_HEADER_FILE_NAME:
+                    HeaderP = Val;
+                    break;
+                case OPTIONS::MODE_IMPLEMENTATON_FILE_NAME:
+                    ImpleP = Val;
+                    break;
+                case OPTIONS::MODE_NO_INCLUDE_GUARD:
+                    m_AdditionalOptions[NO_INCLUDE_GUARD] = 1;
+                    break;
+                case OPTIONS::MODE_NO_VERBOSE:
+                    m_AdditionalOptions[NO_VERB] = 1;
+                    break;
+                case OPTIONS::MODE_NO_INCLUDE:
+                    m_AdditionalOptions[NO_INCLUDE] = 1;
+                    break;
+                case OPTIONS::MODE_HELP:
+                    cout << HELPERMSG;
+                    SUCCESS_EXIT();
+                    break;
+                case OPTIONS::MODE_IMPLE_STRUCT:
+                    m_AdditionalOptions[IMPLE_STRUCT] = 1;
+                    break;
+                default:
+                    FAILED_EXIT();
+                    break;
+                }
+            }
+        }
+    }
+
+    bool IsArg(char *str)
+    {
+        if (str[0] == '-')
+            return true;
+        else
+            return false;
+    }
+
+    OPTIONS ProcessArguements(const std::string &mode, const std::string &value)
+    {
+        if (mode == "help")
+        {
+            return OPTIONS::MODE_HELP;
+        }
+
+        if (value[0] == '-')
+        {
+            // FAILED_EXIT();
+        }
+        if (mode == "header")
+        {
+            return OPTIONS::MODE_HEADER_FILE_NAME;
+        }
+        if (mode == "imple")
+        {
+            return OPTIONS::MODE_IMPLEMENTATON_FILE_NAME;
+        }
+        if (mode == "novb")
+        {
+            return OPTIONS::MODE_NO_VERBOSE;
+        }
+        if (mode == "noguard")
+        {
+            return OPTIONS::MODE_NO_INCLUDE_GUARD;
+        }
+        if (mode == "noinclude")
+        {
+            return OPTIONS::MODE_NO_INCLUDE;
+        }
+        if (mode == "implestruct")
+        {
+            return OPTIONS::MODE_IMPLE_STRUCT;
+        }
+        else
+        {
+            return OPTIONS::MODE_INVALID_ARG;
+        }
+    }
+    void WriteToFile()
+    {
+        auto file = utils::ReadFile(HeaderP);
+        auto lines = utils::TokeniseStringToLines(file);
+
+        std::vector<std::string> FormattedFunc;
+        CMI cmi(lines, m_AdditionalOptions);
+
+        auto pfs = cmi.GetParsedFunctions();
+        if (!m_AdditionalOptions[NO_VERB])
+            std::cout << "Detected functions:\n";
+
+        int i = 1;
+
+        for (auto &function : pfs)
+        {
+            std::stringstream ss;
+
+            ss << function.Rtype << " "
+               << function.ClassName << "::" << function.Name << "(" << function.Args << ")"
+               << (function.IsConst ? " const" : "");
+            if (!m_AdditionalOptions[NO_VERB])
+                std::cout << i << ". " << ss.str() << '\n';
+
+            FormattedFunc.push_back(ss.str());
+
+            i++;
+        }
+
+        cmi.GenerateCMI(FormattedFunc, ImpleP, HeaderP);
+    }
+};
+int main(int argc, char **argv) { application::Start(argc, argv); }
